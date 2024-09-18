50df8287586b3fbaf65505c85b527cde5a862991
Mihai Calin Luca <mihaicalin.luca10@gmail.com>
2024-09-18 12:43:33 +0200

    cleanup and context refresh + optimization

--- frontend/src/context.rs
+++ frontend/src/context.rs
@@ -1,8 +1,8 @@
+use crate::config::Config;
+use crate::requests::query;
+use html::ChildrenProps;
 use std::cell::RefCell;
 use std::rc::Rc;
-
-use crate::config::Config;
-use html::ChildrenProps;
 use yew::prelude::*;
 
 #[derive(Clone, Debug, PartialEq)]
@@ -11,6 +11,16 @@ pub struct ConfigContext {
     pub deadline: String,
     pub set_deadline: Callback<String>,
     pub set_config: Callback<Config>,
+}
+
+pub async fn refresh_context() -> (Config, String) {
+    let config = Config::new();
+    let mut deadline = String::new();
+    if let Ok(new_deadline) = query::get_deadline(&config).await {
+        deadline = new_deadline;
+    }
+
+    (config, deadline)
 }
 
 impl Default for ConfigContext {
@@ -26,7 +36,7 @@ impl Default for ConfigContext {
 
 #[function_component(ConfigProvider)]
 pub fn config_provider(props: &ChildrenProps) -> Html {
-    let config = use_state(|| Rc::new(RefCell::new(Config::new())));
+    let config = use_state(Config::new);
     let deadline = use_state(String::new);
 
     let set_deadline = {
@@ -39,13 +49,36 @@ pub fn config_provider(props: &ChildrenProps) -> Html {
     let set_config = {
         let config = config.clone();
         Callback::from(move |new_config: Config| {
-            let mut config = config.borrow_mut();
-            *config = new_config;
+            config.set(new_config);
         })
     };
 
+    let set_deadline_effect = set_deadline.clone();
+    let set_config_effect = set_config.clone();
+
+    // refresh context on component mount
+    use_effect_with_deps(
+        move |_| {
+            wasm_bindgen_futures::spawn_local(async move {
+                let (new_config, new_deadline) = refresh_context().await;
+
+                set_deadline_effect.emit(new_deadline);
+                set_config_effect.emit(new_config);
+            });
+            || () // no cleanup fn
+        },
+        (), // empty dependency array, run once on mount
+    );
+
+    let context = ConfigContext {
+        config: Rc::new(RefCell::new((*config).clone())),
+        deadline: (*deadline).clone(),
+        set_deadline,
+        set_config,
+    };
+
     html! {
-        <ContextProvider<ConfigContext> context={ConfigContext { config: (*config).clone(), deadline: (*deadline).clone(), set_deadline, set_config}}>
+        <ContextProvider<ConfigContext> context={context}>
             { for props.children.iter() }
         </ContextProvider<ConfigContext>>
     }
